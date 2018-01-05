99807b810831f6319c736c1fda7165c785e7eb46
Taewoong Moon <moondingie5@gmail.com>
2018-01-05 19:15:16 +0900

    initial commit

---
+++ src/components/Taewoong.vue
@@ -0,0 +1,24 @@
+<template lang="html">
+  <div class="test">
+    <h3>이름: {{name}}</h3>
+    <h3>나이: {{age}}</h3>
+    <h3>도시: {{city}}</h3>
+    <router-link to="/">Go to Foo</router-link>
+
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: 'Taewoong',
+      age: 22,
+      city: 'Anyang',
+    }
+  }
+}
+</script>
+
+<style lang="css">
+</style>
